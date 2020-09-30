17734fca34b9a01fab4bf632af85fe25793b63cc
Chris Chiu <chiu@endlessos.org>
2020-09-30 10:20:15 +0800

    rtl8xxxu: add handle for mac80211 get_txpower
    add .get_txpower handle for mac80211 operations for `iw` and `wext`
    tools to get the underlying tx power (max limit).
    
    Signed-off-by: Chris Chiu <chiu@endlessos.org>
    (cherry picked from commit 31d27d8b9f97dab7cb93f0fe89bf9b21c5a120d0)

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -1414,6 +1414,52 @@ rtl8xxxu_gen1_dbm_to_txpwridx(struct rtl8xxxu_priv *priv, u16 mode, int dbm)
         return txpwridx;
 }
 
+static int
+rtl8xxxu_gen1_txpwridx_to_dbm(struct rtl8xxxu_priv *priv, u16 mode, u8 idx)
+{
+        int offset;
+        int pwrout_dbm;
+
+        switch (mode) {
+        case WIRELESS_MODE_B:
+                offset = -7;
+                break;
+        case WIRELESS_MODE_G:
+        case WIRELESS_MODE_N_24G:
+                offset = -8;
+                break;
+        default:
+                offset = -8;
+                break;
+        }
+        pwrout_dbm = idx / 2 + offset;
+        return pwrout_dbm;
+}
+
+int
+rtl8xxxu_gen1_get_tx_power(struct rtl8xxxu_priv *priv)
+{
+	u8 txpwr_level;
+	int txpwr_dbm;
+
+	txpwr_level = priv->cur_cck_txpwridx;
+	txpwr_dbm = rtl8xxxu_gen1_txpwridx_to_dbm(priv, WIRELESS_MODE_B,
+						  txpwr_level);
+	txpwr_level = priv->cur_ofdm24g_txpwridx +
+		      priv->ofdm_tx_power_index_diff[1].a;
+
+	if (rtl8xxxu_gen1_txpwridx_to_dbm(priv, WIRELESS_MODE_G, txpwr_level)
+	    > txpwr_dbm)
+		txpwr_dbm = rtl8xxxu_gen1_txpwridx_to_dbm(priv, WIRELESS_MODE_G,
+						          txpwr_level);
+	txpwr_level = priv->cur_ofdm24g_txpwridx;
+	if (rtl8xxxu_gen1_txpwridx_to_dbm(priv, WIRELESS_MODE_N_24G,
+					  txpwr_level) > txpwr_dbm)
+		txpwr_dbm = rtl8xxxu_gen1_txpwridx_to_dbm(priv, WIRELESS_MODE_N_24G, txpwr_level);
+
+	return txpwr_dbm;
+}
+
 void
 rtl8xxxu_gen1_set_tx_power(struct rtl8xxxu_priv *priv, int channel, bool ht40)
 {
@@ -4540,13 +4586,26 @@ rtl8xxxu_wireless_mode(struct ieee80211_hw *hw, struct ieee80211_sta *sta)
 	return network_type;
 }
 
+static int rtl8xxxu_get_txpower(struct ieee80211_hw *hw,
+				struct ieee80211_vif *vif, int *dbm)
+{
+	struct rtl8xxxu_priv *priv = hw->priv;
+
+	if (!priv->fops->get_tx_power)
+		return -EOPNOTSUPP;
+
+	*dbm = priv->fops->get_tx_power(priv);
+
+	return 0;
+}
+
 static void rtl8xxxu_update_txpower(struct rtl8xxxu_priv *priv, int power)
 {
 	bool ht40 = false;
 	struct ieee80211_hw *hw = priv->hw;
 	int channel = hw->conf.chandef.chan->hw_value;
 	u8 cck_txpwridx, ofdm_txpwridx;
-	int i;
+	int i, group;
 
 	if (!priv->fops->dbm_to_txpwridx)
 		return;
@@ -4570,7 +4629,7 @@ static void rtl8xxxu_update_txpower(struct rtl8xxxu_priv *priv, int power)
 						    power);
 
 	if (ofdm_txpwridx - priv->ofdm_tx_power_index_diff[1].a > 0)
-		ofdm_txpwridx -= priv->ofdm_tx_power_index_diff[1].a;
+		ofdm_txpwridx -= priv->ofdm_tx_power_index_diff[1].a;	// refer to rtlefuse->legacy_ht_txpowerdiff in vendor driver
 	else
 		ofdm_txpwridx = 0;
 
@@ -4582,6 +4641,10 @@ static void rtl8xxxu_update_txpower(struct rtl8xxxu_priv *priv, int power)
 		priv->ht40_1s_tx_power_index_A[i] = ofdm_txpwridx;
 		priv->ht40_1s_tx_power_index_B[i] = ofdm_txpwridx;
 	}
+
+	group = rtl8xxxu_gen1_channel_to_group(channel);
+	priv->cur_cck_txpwridx = priv->cck_tx_power_index_A[group];
+	priv->cur_ofdm24g_txpwridx = priv->ht40_1s_tx_power_index_A[group];
 
 	priv->fops->set_tx_power(priv, channel, ht40);
 }
@@ -6540,6 +6603,7 @@ static const struct ieee80211_ops rtl8xxxu_ops = {
 	.set_key = rtl8xxxu_set_key,
 	.ampdu_action = rtl8xxxu_ampdu_action,
 	.sta_statistics = rtl8xxxu_sta_statistics,
+	.get_txpower = rtl8xxxu_get_txpower,
 };
 
 static int rtl8xxxu_parse_usb(struct rtl8xxxu_priv *priv,
