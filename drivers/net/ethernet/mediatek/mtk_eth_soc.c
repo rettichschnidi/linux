fd094889fd1452919cdaa53d959994927ce91004
Reto Schneider <reto.schneider@husqvarnagroup.com>
2021-06-05 12:46:24 +0200

    net: ethernet: mtk_eth_soc: Support custom ifname
    Name the MAC interface name according to the label property. If the
    property is missing, the default name (ethX) gets used.
    
    labels with more than IFNAMSIZ -1 characters will be silently truncated,
    which seems to be what most of the code base does when using strscpy.
    
    Signed-off-by: Reto Schneider <reto.schneider@husqvarnagroup.com>
    
    Series-to: devicetree@vger.kernel.org
    Series-to: linux-mediatek@lists.infradead.org
    Series-to: netdev@vger.kernel.org
    Series-cc: Stefan
    Series-version: 2
    Series-changes: 2
    
    - Avoid dangerous usage of strncpy

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -2828,6 +2828,7 @@ static const struct net_device_ops mtk_netdev_ops = {
 static int mtk_add_mac(struct mtk_eth *eth, struct device_node *np)
 {
 	const __be32 *_id = of_get_property(np, "reg", NULL);
+	const char *const name = of_get_property(np, "label", NULL);
 	phy_interface_t phy_mode;
 	struct phylink *phylink;
 	struct mtk_mac *mac;
@@ -2899,6 +2900,9 @@ static int mtk_add_mac(struct mtk_eth *eth, struct device_node *np)
 	}
 
 	mac->phylink = phylink;
+
+	if (name)
+		strscpy(eth->netdev[id]->name, name, IFNAMSIZ);
 
 	SET_NETDEV_DEV(eth->netdev[id], eth->dev);
 	eth->netdev[id]->watchdog_timeo = 5 * HZ;
